ec28cb795513061b30c8fcda61063400dec21069
Abi Yasid Al'Majid <142866105+AbiYasidAlMajid2300011@users.noreply.github.com>
2025-02-16 21:38:59 +0700

    Add files via upload

---
+++ JumlahASCII.java
@@ -0,0 +1,21 @@
+package com.abiyasidalmajid2300011.tugaspertemuan2;
+import java.util.Scanner;
+
+public class JumlahASCII {
+    public static void main(String[] args) {
+        Scanner MyInput = new Scanner(System.in);
+        char karakter1, karakter2;
+        
+        System.out.print("Masukkan karakter pertama: ");
+        karakter1 = MyInput.next().charAt(0);
+        System.out.print("Masukkan karakter kedua: ");
+        karakter2 = MyInput.next().charAt(0);
+        
+        int ascii1 = karakter1;
+        int ascii2 = karakter2;
+        
+        System.out.println("Kode ASCII '1': " + ascii1);
+        System.out.println("Kode ASCII '2': " + ascii2);
+        System.out.println("Jumlah dari kode ASCII '1' dan '2': " + ascii1 + " + " + ascii2 + " = " + (ascii1 + ascii2));
+    }
+}
